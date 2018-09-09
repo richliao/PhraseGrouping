bdcc3f5231ad0510a2063db7f1a12c75785925ec
Zhi Liao <zhiliao@Zhis-iMac.fios-router.home>
2018-09-09 19:57:52 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import 'react-sortable-tree/styles.css';
+import 'react-sortable-tree/styles.css';
 import KeyWordsTree from './KeyWordsTree';
 
 const title = 'Initial example of react sortable tree';
 
 ReactDOM.render(
-  <div>{title}</div>,
-  <KeyWordsTree/>,
+  <KeyWordsTree />,
   document.getElementById('app')
 );
 
